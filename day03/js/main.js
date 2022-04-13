617d53308b7f97ca5edd23049086cf1e82106a17
vinferno <vinsonfernandez@gmail.com>
2022-04-12 21:06:46 -0700

    day3

--- day03/js/main.js
+++ day03/js/main.js
@@ -3,9 +3,9 @@ const h3Tags = document.querySelectorAll('h3');
 
 console.log(pTags, h3Tags);
 
-h3Tags[0].addEventListener('scroll', function() {
+h3Tags[0].addEventListener('click', function() {
     pTags[0].classList.toggle('visible');
 });
-h3Tags[1].addEventListener('mouseleave', function() {
+h3Tags[1].addEventListener('click', function() {
     pTags[1].classList.toggle('visible');
 });
